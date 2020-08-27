3e0583fb6335b870535d22519c4b52829afeb13e
work <work@Jonathans-MacBook-Air.local>
2020-08-28 08:48:20 +1000

    mysql deployed on cloud

--- database.js
+++ database.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql');
+const cleardb= 'CLEARDB_ONYX_URL: mysql://bcabfc9f759312:8273d709@us-cdbr-east-02.cleardb.com/heroku_8fbee8686ff81c6?reconnect=true'
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Beautiful#3",
-    database: 'users'
+    host: "us-cdbr-east-02.cleardb.com",
+    user: "bcabfc9f759312",
+    password: "8273d709",
+    database: 'heroku_8fbee8686ff81c6'
   });
   db.connect(function(err) {
     if (err) throw err;
