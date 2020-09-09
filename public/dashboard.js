b8a0d681c2461a121308508a4640a07d9a042be6
work <work@Jonathans-MacBook-Air.local>
2020-09-10 06:48:47 +1000

    changed friendList

--- public/dashboard.js
+++ public/dashboard.js
@@ -29,6 +29,7 @@ if (xhttp.readyState === 4 && xhttp.response == 'user_added') {
    const socket = io('/');
    console.log('dashboard')
    socket.emit('join-dashboard')
+   let friendsList;
    const friendsOnClick = document.querySelectorAll('.dashboard__searchResult')
    const friendsOnClickArray = [...friendsOnClick];
    for (let i =0; i<friendsOnClickArray.length; i++){
